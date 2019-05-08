b14ca07eec1111a9ef589a0d874665ae36d1aee7
TRIF3X <thewizardofcrypto@gmail.com>
2019-05-08 17:21:44 -0500

    Add datasets for men/women clothes

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Enter the URL you want to scrape
-url = 'https://www2.hm.com/en_us/men/products/shirts.html'
+url = 'https://www2.hm.com/en_us/women/products/jumpsuits.html'
 # Perform GET request to URL, timeout in seconds; note: if timeout is omitted, the request will sit indefintely if no response.
 agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 response = requests.get(url, timeout=5, headers=agent)
@@ -26,7 +26,7 @@ for product in content.findAll('li', class_="product-item"):
 
 
 # open with file name where you want the content to go, 'w' will be used to write to this file
-with open('menShirts.json', 'w') as outfile:
+with open('./WomensData/womenJumpSuits.json', 'w') as outfile:
     # Convert data into JSON format
     # https://docs.python.org/3/library/json.html
     json.dump(scrapedData, outfile)
